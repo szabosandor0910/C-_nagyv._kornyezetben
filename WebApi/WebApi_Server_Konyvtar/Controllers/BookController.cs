329ce79210f8392996f60cfb36e79a2862b786bd
ujbalazs <tucika3@gmail.com>
2022-05-28 16:03:09 +0200

    webapi kész

--- WebApi/WebApi_Server_Konyvtar/Controllers/BookController.cs
+++ WebApi/WebApi_Server_Konyvtar/Controllers/BookController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebApi_Common.Models;
+using WebApi_Server_Konyvtar.Repositories;
 
 namespace WebApi_Server_Konyvtar.Controllers
 {
@@ -12,9 +13,93 @@ namespace WebApi_Server_Konyvtar.Controllers
     [Route("api/book")]
     public class BookController : Controller
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<Book>> Get() {
-            return Ok(new[] { new Book { Id = 1, Title = "tesztcím", Loaned = true, StartDate = DateTime.Now, EndDate = DateTime.Now, WhoLoan = "Adrfs" } });
+        private long GetNewId(IEnumerable<Book> library)
+        {
+            long newId = 0;
+            foreach (var book in library)
+            {
+                if (newId < book.Id)
+                {
+                    newId = book.Id;
+                }
+            }
+
+            return newId + 1;
         }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> Get()
+        {
+
+            var library = BookRepository.GetLibrary();
+            return Ok(library);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> Get(int id)
+        {
+            var library = BookRepository.GetLibrary();
+
+            var book = library.FirstOrDefault(x => x.Id == id);
+            if (book != null)
+            {
+                return Ok(library);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Book book)
+        {
+            var Library = BookRepository.GetLibrary().ToList();
+
+            book.Id = GetNewId(Library);
+            Library.Add(book);
+
+            BookRepository.StoreLibrary(Library);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Book book)
+        {
+            var Library = BookRepository.GetLibrary().ToList();
+
+            var BookToUpdate = Library.FirstOrDefault(p => p.Id == book.Id);
+            if (BookToUpdate != null)
+            {
+                BookToUpdate.Title = book.Title;
+                BookToUpdate.Loaned = book.Loaned;
+                BookToUpdate.WhoLoan = book.WhoLoan;
+                BookToUpdate.StartDate = book.StartDate;
+                BookToUpdate.EndDate = book.EndDate;
+
+                BookRepository.StoreLibrary(Library);
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var Library = BookRepository.GetLibrary().ToList();
+
+            var BookToDelete = Library.FirstOrDefault(p => p.Id == id);
+            if (BookToDelete != null)
+            {
+                Library.Remove(BookToDelete);
+
+                BookRepository.StoreLibrary(Library);
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+
     }
 }
---
+++ WebApi/WebApi_Server_Konyvtar/Repositories/BookRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using WebApi_Common.Models;
+
+
+namespace WebApi_Server_Konyvtar.Repositories
+{
+    public static class BookRepository
+    {
+        private const string filename = "Book.json";
+
+        public static IEnumerable<Book> GetLibrary()
+        {
+            if (File.Exists(filename))
+            {
+                var rawData = File.ReadAllText(filename);
+                var library = JsonSerializer.Deserialize<IEnumerable<Book>>(rawData);
+                return library;
+            }
+
+            return new List<Book>();
+        }
+
+        public static void StoreLibrary(IEnumerable<Book> library)
+        {
+            var rawData = JsonSerializer.Serialize(library);
+            File.WriteAllText(filename, rawData);
+        }
+    }
+}
